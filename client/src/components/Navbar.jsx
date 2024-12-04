eefc96334b1713dc9f840c423c0ab65f6de312ea
pruthvi <pruthvirajbarbole55@gmail.com>
2024-12-05 01:09:07 +0530

    v24

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 text-white p-4">
+    <nav className="bg-red-600 text-gray-200 p-4">
       <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between">
         <div className="text-2xl font-bold mb-2 sm:mb-0">
           <Link to="/">Task Scheduler</Link>
         </div>
         <div className="flex space-x-4">
-          <Link to="/" className="hover:text-gray-200">
+          <Link to="/" className="hover:text-gray-400">
             View Tasks
           </Link>
-          <Link to="/create" className="hover:text-gray-200">
+          <Link to="/create" className="hover:text-gray-400">
             Create Task
           </Link>
         </div>
